60681f043ac76f5d0df16698885b97a8383cf904
Tim Costrop <tim.costrop@roots.be>
2024-03-25 21:53:54 +0100

    ✨ made the QR code transparent.

--- src/test/java/be/costrop/rsvp/GenerateQrCodeInTest.java
+++ src/test/java/be/costrop/rsvp/GenerateQrCodeInTest.java
@@ -1,26 +1,35 @@
 package be.costrop.rsvp;
 
 import com.google.zxing.BarcodeFormat;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.qrcode.QRCodeWriter;
 import org.junit.jupiter.api.Test;
 
 import javax.imageio.ImageIO;
-import java.io.File;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
 
 public class GenerateQrCodeInTest {
+	private static final int SIZE = 100;
+
 	@Test
 	void shouldIWantAQrCode_generateOne() {
-		final var qrCodeContent = "https://docs.google.com/forms/d/e/1FAIpQLScsBC1_jgtbtrCw-CBKCSpyVqS1DjFTq4mUmabqiI_fdrRsLA/viewform";
+		final var qrCodeContent = "https://tim-en-laura-anne-trouwen.be";
 
 		System.out.println("Encoding QR code with following content " + qrCodeContent);
 
 		final var qrCodeWriter = new QRCodeWriter();
 
 		try {
-			final var bitMatrix = qrCodeWriter.encode(qrCodeContent, BarcodeFormat.QR_CODE, 100, 100);
-			final var outputFile = new File("target/qr.png");
-			ImageIO.write(MatrixToImageWriter.toBufferedImage(bitMatrix), "png", outputFile);
+			final var bitMatrix = qrCodeWriter.encode(qrCodeContent, BarcodeFormat.QR_CODE, SIZE, SIZE);
+			final var image = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_ARGB);
+			for (int i = 0; i < SIZE; i++) {
+				for (int j = 0; j < SIZE; j++) {
+					image.setRGB(i, j, bitMatrix.get(i, j) ? Color.BLACK.getRGB() : Color.TRANSLUCENT);
+				}
+			}
+			final var outStream = new FileOutputStream("target/qr.png");
+			ImageIO.write(image, "png", outStream);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}

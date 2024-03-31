8bf1203d5b58f61b10ae7986f318906bed694a9b
Tim Costrop <tim.costrop@roots.be>
2024-03-31 22:42:22 +0200

    💄 Use the font from the invite

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,32 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: 'class',
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./node_modules/primereact/**/*.{js,ts,jsx,tsx}"
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                "sans": ["Montserrat"],
-                "regular": ["Montserrat"],
+	darkMode: 'class',
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./node_modules/primereact/**/*.{js,ts,jsx,tsx}"
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				"sans": ["Merriweather"],
+				"regular": ["Merriweather"],
 				"garton": ["Garton"],
-				"chalkboy": ["Chalkboy"],
-				"parisienne": ["Parisienne"],
-				"prata": ["Prata"],
-            },
-            colors: {
+				// "milkshake": [""]
+			},
+			colors: {
 				"green": 'var(--text-green)',
 				"orange": 'var(--text-orange)',
 				"red": 'var(--text-red)'
-            },
+			},
 			backgroundImage: {
 				'texture': 'url(/img/Background.png)',
 				'header': 'url(/img/header.png)'
 			}
-        },
-    },
-    plugins: [],
+		},
+	},
+	plugins: [],
 }
 
